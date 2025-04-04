526fa2761b06f2f13b979ab9387ef52ba52a3030
Paula Guadalajara <pgs68@alu.ua.es>
2025-04-04 20:06:04 +0200

    modificacion en init

--- scripts/tests/__init__.py
+++ scripts/tests/__init__.py
@@ -1,1 +1,3 @@
 # -*- coding: utf-8 -*-
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non ex finibus, vestibulum erat lobortis, aliquet quam. Nam bibendum diam eget dui pretium gravida. Curabitur vitae vehicula magna. Fusce posuere et arcu non dignissim. Nunc ultricies euismod augue, id luctus purus. Vestibulum eget est sit amet metus malesuada vehicula at ut risus. Sed vestibulum porttitor nulla, ut varius ante. Donec eget accumsan enim.
